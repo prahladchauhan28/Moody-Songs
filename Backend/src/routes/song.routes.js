04120f0e7977a762e1f36daa705c9d71224d9986
prahladchauhan28 <prahladchouhan928@gmail.com>
2025-07-20 17:48:20 +0530

    updates git fix

--- Backend/src/routes/song.routes.js
+++ Backend/src/routes/song.routes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const multer = require('multer');
 const uploadImage = require('../services/storage.service'); 
+const songModel = require('../models/song.model');
 
 const upload =multer({storage: multer.memoryStorage()});
 
@@ -11,9 +12,15 @@ router.post('/songs',upload.single("audio"), async (req, res) => {
       console.log( req.file);
       const fileData = await uploadImage(req.file);
       console.log('File uploaded successfully:', fileData);
+      const song = await songModel.create({
+        title: req.body.title,
+        artist: req.body.artist,
+        audio: fileData.url, 
+        mood: req.body.mood
+      });
       
     res.status(201).json({ message: 'Song data received successfully',
-        song: req.body
+        song: song
      });
 })
 
--- Backend/src/services/storage.service.js
+++ Backend/src/services/storage.service.js
@@ -1,6 +1,7 @@
 const dotenv = require("dotenv");
 dotenv.config();
 const ImageKit = require("imagekit");
+const mangoose = require("mongoose");
 
 var imagekit = new ImageKit({
     publicKey : process.env.IMAGEKIT_PUBLIC_KEY,
@@ -12,7 +13,8 @@ function uploadImage(file) {
     return new Promise((resolve, reject) => {
     imagekit.upload({
         file: file.buffer, 
-        fileName: "cohort-01",
+        fileName: new mangoose.Types.ObjectId().toString(),
+        folder: "Moody-playlist",
     }, (error, result) => {
         if (error) {    
             reject(error);
